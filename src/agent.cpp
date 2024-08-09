fd373e5f60f628304e178634f22e742a66e293d6
soroush <sorooshm78@gmail.com>
2024-08-09 12:42:58 +0000

    add echo call back

--- src/agent.cpp
+++ src/agent.cpp
@@ -31,21 +31,24 @@ class MyCall : public Call {
             }
         }
 
-        virtual void onCallMediaState(OnCallMediaStateParam &prm){
+        void onCallMediaState(OnCallMediaStateParam &prm)
+        {
             CallInfo ci = getInfo();
+
             for (unsigned i = 0; i < ci.media.size(); i++) {
                 if (ci.media[i].type==PJMEDIA_TYPE_AUDIO) {
                     try {
                         AudioMedia aud_med = getAudioMedia(i);
-                        
-                        AudioMediaRecorder wav_writer;
-                        wav_writer.createRecorder("file.wav");
-                        aud_med.startTransmit(wav_writer);
+
+                        // Connect the call audio media to sound device
+                        AudDevManager& mgr = Endpoint::instance().audDevManager();
+                        aud_med.startTransmit(mgr.getPlaybackDevMedia());
+                        mgr.getCaptureDevMedia().startTransmit(aud_med);
                     }
                     catch(const Error &e) {
                         // Handle invalid or not audio media error here
                     }
-                }   
+                }
             }
         }
 };
