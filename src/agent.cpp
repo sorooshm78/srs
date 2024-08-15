9e1a5cfaefcb05bde0114e3ba4ef9e76588c4cab
soroush <sorooshm78@gmail.com>
2024-08-15 08:24:05 +0000

    refactor and clean code

--- src/agent.cpp
+++ src/agent.cpp
@@ -11,8 +11,8 @@ class MyCall : public Call {
     public:
         AudioMediaRecorder recorder;
         
-        MyCall(Account &acc, int call_id = PJSUA_INVALID_ID)
-        : Call(acc, call_id)
+        MyCall(Account &account, int callID = PJSUA_INVALID_ID)
+        : Call(account, callID)
         { }
 
         void print_call_state(string state, string localUri, string remoteUri, long connectDuration, string callID){
@@ -26,113 +26,107 @@ class MyCall : public Call {
             std::cout << std::endl;
         }
 
-        virtual void onCallState(OnCallStateParam &prm){
+        string getWavFileName(){
+            CallInfo callInfo = getInfo();
+            return callInfo.callIdString + ".wav";
+        }
+
+        void saveAudioMedio(AudioMedia audioMedio){
+            string fileName = getWavFileName();
+            recorder.createRecorder(fileName);
+            audioMedio.startTransmit(recorder);
+        }
+
+        void echoVoice(AudioMedia audioMedia){
+            AudDevManager& manager = Endpoint::instance().audDevManager();
+            audioMedia.startTransmit(manager.getPlaybackDevMedia());
+            manager.getCaptureDevMedia().startTransmit(audioMedia);
+
+        }
+
+        virtual void onCallState(OnCallStateParam &param){
             CallInfo callInfo = getInfo();
             if (callInfo.state == PJSIP_INV_STATE_CONNECTING || callInfo.state == PJSIP_INV_STATE_DISCONNECTED){
                 print_call_state(callInfo.stateText, callInfo.localUri, callInfo.remoteUri, callInfo.connectDuration.sec, callInfo.callIdString);
             }
         }
   
-        // save media to wav file
         virtual void onCallMediaState(OnCallMediaStateParam &params)
         {
             CallInfo callInfo = getInfo();
             for (unsigned i = 0; i < callInfo.media.size(); i++)
             {
-                if (callInfo.media[i].type == PJMEDIA_TYPE_AUDIO && getMedia(i))
+                if (callInfo.media[i].type == PJMEDIA_TYPE_AUDIO)
                 {
-                    AudioMedia *audioMedia = (AudioMedia *)getMedia(i);
-                    string file_name = callInfo.callIdString + ".wav";
-                    recorder.createRecorder(file_name);
-                    audioMedia->startTransmit(recorder);
+                    AudioMedia audioMedia = getAudioMedia(i);
+                    saveAudioMedio(audioMedia);
+                    echoVoice(audioMedia);
                 }
             }
         }
-        
-        // // echo call
-        // void onCallMediaState(OnCallMediaStateParam &prm)
-        // {
-        //     CallInfo callInfo = getInfo();
-
-        //     for (unsigned i = 0; i < callInfo.media.size(); i++) {
-        //         if (callInfo.media[i].type==PJMEDIA_TYPE_AUDIO) {
-        //             try {
-        //                 AudioMedia aud_med = getAudioMedia(i);
-                        
-        //                 // Connect the call audio media to sound device
-        //                 AudDevManager& mgr = Endpoint::instance().audDevManager();
-        //                 aud_med.startTransmit(mgr.getPlaybackDevMedia());
-        //                 mgr.getCaptureDevMedia().startTransmit(aud_med);
-        //             }
-        //             catch(const Error &e) {
-        //                 // Handle invalid or not audio media error here
-        //             }
-        //         }
-        //     }
-        // }
 };
 
 // Subclass to extend the Account and get notifications etc.
 class MyAccount : public Account {
     public:
-        virtual void onRegState(OnRegStateParam &prm) {
+        virtual void onRegState(OnRegStateParam &param) {
             AccountInfo ai = getInfo();
             std::cout << (ai.regIsActive? "*** Register:" : "*** Unregister:")
-                    << " code=" << prm.code << std::endl;
+                    << " code=" << param.code << std::endl;
         }
-        virtual void onIncomingCall(OnIncomingCallParam &iprm){
-            MyCall *call = new MyCall(*this, iprm.callId);
-            CallOpParam prm;
-            prm.statusCode = PJSIP_SC_OK;
-            call->answer(prm);
+        virtual void onIncomingCall(OnIncomingCallParam &incomingParam){
+            MyCall *call = new MyCall(*this, incomingParam.callId);
+            CallOpParam param;
+            param.statusCode = PJSIP_SC_OK;
+            call->answer(param);
         }
 };
 
 
-void signal_callback_handler(int signum) {
+void signalCallbackHandler(int signum) {
     is_shutdown = true;
 }
 
 int main()
 {
-    Endpoint ep;
+    Endpoint endpoint;
 
-    ep.libCreate();
+    endpoint.libCreate();
 
     // Initialize endpoint
-    EpConfig ep_cfg;
-    ep.libInit( ep_cfg );
+    EpConfig endpointConfig;
+    endpoint.libInit(endpointConfig);
 
     // Create SIP transport. Error handling sample is shown
-    TransportConfig tcfg;
-    tcfg.port = 6060;
+    TransportConfig transportConfig;
+    transportConfig.port = 6060;
     try {
-        ep.transportCreate(PJSIP_TRANSPORT_UDP, tcfg);
+        endpoint.transportCreate(PJSIP_TRANSPORT_UDP, transportConfig);
     } catch (Error &err) {
         std::cout << err.info() << std::endl;
         return 1;
     }
 
     // Start the library (worker threads etc)
-    ep.libStart();
+    endpoint.libStart();
     std::cout << "*** PJSUA2 STARTED ***" << std::endl;
 
     // Configure an AccountConfig
-    AccountConfig acfg;
-    acfg.idUri = "sip:1000@192.168.228.130:5060";
-    acfg.regConfig.registrarUri = "sip:192.168.228.130:5060";
+    AccountConfig accountConfig;
+    accountConfig.idUri = "sip:1000@192.168.228.130:5060";
+    accountConfig.regConfig.registrarUri = "sip:192.168.228.130:5060";
 
     // Create the account
-    MyAccount *acc = new MyAccount;
-    acc->create(acfg);
+    MyAccount *account = new MyAccount;
+    account->create(accountConfig);
 
-    signal(SIGINT, signal_callback_handler);
+    signal(SIGINT, signalCallbackHandler);
     while(!is_shutdown){
        sleep(1);
     }
 
     // Delete the account. This will unregister from server
-    delete acc;
+    delete account;
 
     return 0;
 }
