960933cd9b08acdeaa9610bf6a7969a8559b0868
Soroush mohammadi <sorooshm78@gmail.com>
2024-10-22 06:40:15 +0000

    add siprec to extension

--- pjproject/pjsip/src/pjsip-ua/sip_siprec.c
+++ pjproject/pjsip/src/pjsip-ua/sip_siprec.c
@@ -16,29 +16,22 @@
 static const pj_str_t STR_SIPREC         = {"siprec", 6};
 
 
+/* Deinitialize siprec */
+static void pjsip_siprec_deinit_module(pjsip_endpoint *endpt)
+{
+    PJ_TODO(provide_initialized_flag_for_each_endpoint);
+    PJ_UNUSED_ARG(endpt);
+}
+
+
 /*
- * Initialize Session Timers support in PJSIP. 
+ * Initialize siprec support in PJSIP. 
  */
 PJ_DEF(pj_status_t) pjsip_siprec_init_module(pjsip_endpoint *endpt)
 {
     pj_status_t status;
 
-    // PJ_ASSERT_RETURN(endpt, PJ_EINVAL);
-
-    // if (is_initialized)
-    //     return PJ_SUCCESS;
-
-    // /* Register Session-Expires header parser */
-    // status = pjsip_register_hdr_parser( STR_SE.ptr, STR_SHORT_SE.ptr, 
-    //                                     &parse_hdr_se);
-    // if (status != PJ_SUCCESS)
-    //     return status;
-
-    // /* Register Min-SE header parser */
-    // status = pjsip_register_hdr_parser( STR_MIN_SE.ptr, NULL, 
-    //                                     &parse_hdr_min_se);
-    if (status != PJ_SUCCESS)
-        return status;
+    PJ_ASSERT_RETURN(endpt, PJ_EINVAL);
 
     /* Register 'siprec' capability to endpoint */
     status = pjsip_endpt_add_capability(endpt, NULL, PJSIP_H_SUPPORTED,
@@ -47,17 +40,15 @@ PJ_DEF(pj_status_t) pjsip_siprec_init_module(pjsip_endpoint *endpt)
     if (status != PJ_SUCCESS)
         return status;
 
-    // /* Register deinit module to be executed when PJLIB shutdown */
-    // if (pjsip_endpt_atexit(endpt, &pjsip_timer_deinit_module) != PJ_SUCCESS) {
-    //     /* Failure to register this function may cause this module won't 
-    //      * work properly when the stack is restarted (without quitting 
-    //      * application).
-    //      */
-    //     pj_assert(!"Failed to register Session Timer deinit.");
-    //     PJ_LOG(1, (THIS_FILE, "Failed to register Session Timer deinit."));
-    // }
-
-    // is_initialized = PJ_TRUE;
+    /* Register deinit module to be executed when PJLIB shutdown */
+    if (pjsip_endpt_atexit(endpt, &pjsip_siprec_deinit_module) != PJ_SUCCESS) {
+        /* Failure to register this function may cause this module won't 
+         * work properly when the stack is restarted (without quitting 
+         * application).
+         */
+        pj_assert(!"Failed to register Session Timer deinit.");
+        PJ_LOG(1, (THIS_FILE, "Failed to register Session Timer deinit."));
+    }
 
     return PJ_SUCCESS;
 }
