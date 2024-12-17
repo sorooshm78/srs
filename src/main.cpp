42b80d46d160b19e160dff166553137678de50d2
Soroush mohammadi <sorooshm78@gmail.com>
2024-12-17 08:25:47 +0000

    diable video

--- src/main.cpp
+++ src/main.cpp
@@ -164,7 +164,7 @@ public:
         CallOpParam param;
         param.statusCode = PJSIP_SC_OK;
         param.opt.audioCount = 2;
-        param.opt.videoCount = 2;
+        param.opt.videoCount = 0;
         call->answer(param);
     }
 };
