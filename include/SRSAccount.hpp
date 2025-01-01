9d14676e13ba06c6a4231aa9b60a9e7e0d1673dc
Soroush mohammadi <sorooshm78@gmail.com>
2025-01-01 07:42:09 +0000

    refactore code

--- include/SRSAccount.hpp
+++ include/SRSAccount.hpp
@@ -5,14 +5,6 @@
 class SRSAccount : public pj::Account
 {
 public:
-    /**
-     * Notify application when registration or unregistration has been
-     * initiated. Note that this only notifies the initial registration
-     * and unregistration. Once registration session is active, subsequent
-     * refresh will not cause this callback to be called.
-     */
-    void onRegState(pj::OnRegStateParam& param);
-    
     /* Notify application on incoming call. */
     void onIncomingCall(pj::OnIncomingCallParam& incomingParam);
 };
--- src/SRSAccount.cpp
+++ src/SRSAccount.cpp
@@ -4,14 +4,7 @@
 #include <iostream>
 
 
-void SRSAccount::onRegState(pj::OnRegStateParam& param)
-{
-    pj::AccountInfo accountInfo = getInfo();
-    std::cout << (accountInfo.regIsActive ? "*** Register:" : "*** Unregister:")
-        << " code=" << param.code << std::endl;
-}
-
-void  SRSAccount::onIncomingCall(pj::OnIncomingCallParam& incomingParam)
+void SRSAccount::onIncomingCall(pj::OnIncomingCallParam& incomingParam)
 {
     SRSCall* call = new SRSCall(*this, incomingParam.callId);
     pj::CallOpParam param;
